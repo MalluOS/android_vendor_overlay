fc35928d2380e1cd8166582caf2cc5293d58f391
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-03-12 14:16:31 +0200

    overlay: Add FOD animation resources
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- config.mk
+++ config.mk
@@ -14,7 +14,11 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common
+DEVICE_PACKAGE_OVERLAYS += vendor/overlays/overlay/common
+ifeq ($(EXTRA_FOD_ANIMATIONS),true)
+DEVICE_PACKAGE_OVERLAYS += vendor/overlays/overlay/fod
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/overlay/fod
+endif
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/overlay/prebuilt/system/fonts,$(TARGET_COPY_OUT_SYSTEM)/fonts)
