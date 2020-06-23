d3084fa6b18b52fdf36332f832be17e233fa0c5a
Pranav Vashi <neobuddy89@gmail.com>
2020-06-24 03:03:35 +0530

    overlays: Re-setup specific overlays
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- config.mk
+++ config.mk
@@ -16,9 +16,15 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 DEVICE_PACKAGE_OVERLAYS += vendor/overlays/overlay/common
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/overlay/common
+
 ifeq ($(EXTRA_FOD_ANIMATIONS),true)
-DEVICE_PACKAGE_OVERLAYS += vendor/overlays/overlay/fod
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/overlay/fod
+DEVICE_PACKAGE_OVERLAYS += vendor/overlays/overlay/fod-animations
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/overlay/fod-animations
+endif
+
+ifeq ($(TARGET_HAS_FOD),true)
+DEVICE_PACKAGE_OVERLAYS += vendor/overlays/overlay/fod-icons
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/overlay/fod-icons
 endif
 
 # Include {Lato,Rubik} fonts
